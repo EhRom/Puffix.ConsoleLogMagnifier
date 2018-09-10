7353b12856a8fa0fad42e807038d16a06cdbbe94
Romain EHRET <romain.ehret@sword-group.com>
2018-09-10 10:46:08 +0200

    Add code

---
+++ Puffix.ConsoleLogMagnifier/ConsoleHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Puffix.ConsoleLogMagnifier
+{
+    /// <summary>
+    /// Helper to magnify logs in your Console Application (.Net and .Net Core.
+    /// </summary>
+    public static class ConsoleHelper
+    {
+        /// <summary>
+        /// Add new line.
+        /// </summary>
+        public static void WriteLine()
+        {
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Display message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static void Write(string message)
+        {
+            CoreWriteMessage(ConsoleColor.White, message);
+        }
+
+        /// <summary>
+        /// Display verbose message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static void WriteVerbose(string message)
+        {
+            CoreWriteMessage(ConsoleColor.Gray, message);
+        }
+
+        /// <summary>
+        /// Display success message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static void WriteSuccess(string message)
+        {
+            CoreWriteMessage(ConsoleColor.Green, message);
+        }
+
+        /// <summary>
+        /// Display information message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static void WriteInfo(string message)
+        {
+            CoreWriteMessage(ConsoleColor.Cyan, message);
+        }
+
+        /// <summary>
+        /// Display warning message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static void WriteWarning(string message)
+        {
+            CoreWriteMessage(ConsoleColor.Yellow, message);
+        }
+
+        /// <summary>
+        /// Display error message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static void WriteError(string message)
+        {
+            CoreWriteMessage(ConsoleColor.Red, message);
+        }
+
+        /// <summary>
+        /// Display error.
+        /// </summary>
+        /// <param name="error">Error.</param>
+        public static void WriteError(Exception error)
+        {
+            CoreWriteMessage(ConsoleColor.Red, $"Date : {DateTime.Now} - Type : {error.GetType()} / Message : {error.Message}");
+            if (error.InnerException != null)
+                WriteError(error.InnerException);
+        }
+
+        /// <summary>
+        /// Display message.
+        /// </summary>
+        /// <param name="color">Foreground (text) color.</param>
+        /// <param name="message">Message.</param>
+        private static void CoreWriteMessage(ConsoleColor color, string message)
+        {
+            ConsoleColor savedColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = savedColor;
+        }
+    }
+}
